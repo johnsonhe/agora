55a738763e82d1b0c9affa4573ef1dfd2dee4fe5
Johnson <johnson.he61@gmail.com>
2021-04-17 20:06:47 -0500

    course control

--- api/server/controllers/course.js
+++ api/server/controllers/course.js
@@ -10,4 +10,50 @@ module.exports = {
       .then(course => res.status(201).send(course))
       .catch(error => res.status(400).send(error));
   },
+  retrieve(req, res) {
+    return Course
+      .findByPk(req.params.courseId)
+      .then(Course => {
+        if (!Course) {
+          return res.status(404).send({
+            message: 'Course Not Found',
+          });
+        }
+        return res.status(200).send(Course);
+      })
+      .catch(error => res.status(400).send(error));
+  },
+  lookup(req, res) {
+    return Course
+      .findAll({
+        where: {
+          name: req.params.name
+        }
+      })
+      .then(Course => {
+        if (!Course) {
+          return res.status(404).send({
+            message: 'Course Not Found',
+          });
+        }
+        return res.status(200).send(Course);
+      })
+      .catch(error => res.status(400).send(error));
+  },
+  destroy(req, res) {
+    return Course
+      .findByPk(req.params.courseId)
+      .then(Course => {
+        if (!Course) {
+          return res.status(400).send({
+            message: 'Course Not Found',
+          });
+        }
+        return Course
+          .destroy()
+          .then(() => res.status(204).send())
+          .catch(error => res.status(400).send(error));
+      })
+      .catch(error => res.status(400).send(error));
+  },
 };
