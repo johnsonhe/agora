7632834585b1a91654b75f2c569c7558f80c67a6
Johnson <johnson.he61@gmail.com>
2021-04-17 19:52:12 -0500

    Update contribution.js

--- api/server/controllers/contribution.js
+++ api/server/controllers/contribution.js
@@ -17,4 +17,38 @@ module.exports = {
       .then(contribution => res.status(200).send(contribution))
       .catch(error => res.status(400).send(error));
   },
+  retrieve(req, res) {
+    return Contribution
+      .findByPk(req.params.contributionId)
+      .then(Contribution => {
+        if (!Contribution) {
+          return res.status(404).send({
+            message: 'Contribution Not Found',
+          });
+        }
+        return res.status(200).send(Contribution);
+      })
+      .catch(error => res.status(400).send(error));
+  },
+  destroy(req, res) {
+    return Contribution
+      .find({
+          where: {
+            id: req.params.id          
+          },
+        })
+      .then(contribution => {
+        if (!contribution) {
+          return res.status(404).send({
+            message: 'ContributionId Not Found',
+          });
+        }
+  
+        return contribution
+          .destroy()
+          .then(() => res.status(204).send())
+          .catch(error => res.status(400).send(error));
+      })
+      .catch(error => res.status(400).send(error));
+  },
 };
