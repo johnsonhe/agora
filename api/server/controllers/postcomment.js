75f9777d4365959db701a776a44d0c075ca279db
Johnson <johnson.he61@gmail.com>
2021-04-17 21:50:08 -0500

    controller stuf

--- api/server/controllers/postcomment.js
+++ api/server/controllers/postcomment.js
@@ -12,4 +12,20 @@ module.exports = {
       .then(comment => res.status(201).send(comment))
       .catch(error => res.status(400).send(error));
   },
+  destroy(req, res) {
+    return PostComment
+      .findByPk(req.params.postCommentId)
+      .then(PostComment => {
+        if (!PostComment) {
+          return res.status(400).send({
+            message: 'Post Comment Not Found',
+          });
+        }
+        return PostComment
+          .destroy()
+          .then(() => res.status(204).send())
+          .catch(error => res.status(400).send(error));
+      })
+      .catch(error => res.status(400).send(error));
+  },
 };
